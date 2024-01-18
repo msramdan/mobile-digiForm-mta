81963153e5137d71cbdef66162da604a88649ae2
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2024-01-18 16:53:04 +0700

    change icon

--- android/app/src/main/java/com/digiform/MainActivity.java
+++ android/app/src/main/java/com/digiform/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "DigiForm";
+    return "DigiForm MTA";
   }
 
   /**
--- ios/DigiFormMTATests/DigiFormTests.m
+++ ios/DigiFormMTATests/DigiFormTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface DigiFormTests : XCTestCase
+@interface DigiFormMTATests : XCTestCase
 
 @end
 
-@implementation DigiFormTests
+@implementation DigiFormMTATests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
