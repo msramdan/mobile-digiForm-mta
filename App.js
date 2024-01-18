14c201f5824c10ee061f6668c9910e7c58f61cbb
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2024-01-18 16:40:03 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <WebView source={{ uri: 'https://alkes.easytopup.my.id/auth-web' }} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
